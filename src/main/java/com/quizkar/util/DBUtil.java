5f556e1b4846acf9d0b19695ae4dcc66887aa547
Omkar Kshirsagar <omkarkshirsagar2005@gmail.com>
2025-04-14 15:00:08 +0530

    testing env variables

--- src/main/java/com/quizkar/util/DBUtil.java
+++ src/main/java/com/quizkar/util/DBUtil.java
@@ -5,30 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-	
-//     Hardcoded Render database credentials
-//    private static final String DB_URL = "jdbc:postgresql://localhost:5432/quizkar?sslmode=disable";
-//    private static final String DB_USER = "root";
-//    private static final String DB_PASS = "root@123";
-
-    
-    private static final String DB_URL = "jdbc:postgresql://ep-small-glade-a1aqhfjk-pooler.ap-southeast-1.aws.neon.tech:5432/quizkar?sslmode=require";
-    private static final String DB_USER = "root";
-    private static final String DB_PASS = "npg_YKH3dG4SInNx";
-
-    //private static final String DB_URL = "jdbc:postgresql://localhost/quizkar_db";
-    //private static final String DB_USER = "dnyaneshwar";
-    //private static final String DB_PASS = "root@3121";
-    
-    
-//    private static final String DATABASE_URL[] = System.getenv("DATABASE_URL").split(",");
-//    
-//    private static final String DB_URL = DATABASE_URL[0];
-//    private static final String DB_USER = DATABASE_URL[1];
-//    private static final String DB_PASS = DATABASE_URL[2];
-//    
-    
-    
+    // Fetch database credentials from environment variables
+    private static final String DB_URL = System.getenv("DB_URL");
+    private static final String DB_USER = System.getenv("DB_USER");
+    private static final String DB_PASS = System.getenv("DB_PASS");
     private static final String DB_DRIVER = "org.postgresql.Driver";
     
     static {
@@ -42,5 +22,4 @@ public class DBUtil {
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
     }
-    
 }
